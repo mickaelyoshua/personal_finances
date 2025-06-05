b33ae861a2489e81a5a3cb87d71d5d5f3725ab2f
mickaelyoshua <mickaelyoshua@gmail.com>
2025-06-05 11:16:31 -0300

    code to test sqlc implementation

--- app/main.go
+++ app/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"reflect"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+
+	"github.com/mickaelyoshua/personal-finances/personal_finances"
+)
+
+func run() error {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, "user=postgres password=postgres dbname=personal_finances host=localhost port=5432 sslmode=verify-full")
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	queries := personal_finances.New(conn)
+
+	insertedUser, err := queries.CreateUser(ctx, personal_finances.CreateUserParams{
+		Name:  "John Doe",
+		Email: "john.doe@example.com",
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("Inserted User: %+v\n", insertedUser)
+
+	user, err := queries.GetUser(ctx, insertedUser.ID)
+	if err != nil {
+		return err
+	}
+	log.Printf("Retrieved User: %+v\n", user)
+
+	if reflect.DeepEqual(insertedUser, user) {
+		log.Println("Inserted user and retrieved user are identical.")
+	} else {
+		log.Println("Inserted user and retrieved user differ.")
+	}
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatalf("Error running application: %v", err)
+	}
 }
