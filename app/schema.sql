d1d0f54cd31a722beb324e611242af2f5df4b898
mickaelyoshua <mickaelyoshua@gmail.com>
2025-06-05 16:29:18 -0300

    finished intial config to sqlc

--- app/schema.sql
+++ app/schema.sql
@@ -1,26 +1,26 @@
-CREATE TABLE expense_categories (
+CREATE TABLE IF NOT EXISTS expense_categories (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE expense_sub_categories (
+CREATE TABLE IF NOT EXISTS expense_sub_categories (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	category_id UUID NOT NULL REFERENCES expense_categories(id) ON DELETE CASCADE,
 	name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE income_categories (
+CREATE TABLE IF NOT EXISTS income_categories (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE income_sub_categories (
+CREATE TABLE IF NOT EXISTS income_sub_categories (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	category_id UUID NOT NULL REFERENCES income_categories(id) ON DELETE CASCADE,
 	name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id SERIAL PRIMARY KEY,
 	email TEXT UNIQUE NOT NULL CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
 	name TEXT NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE users (
 	deleted_at TIMESTAMPTZ DEFAULT NULL
 );
 
-CREATE TABLE expenses (
+CREATE TABLE IF NOT EXISTS expenses (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER NOT NULL CONSTRAINT fk_expenses_user REFERENCES users(id) ON DELETE CASCADE,
 	sub_category_id UUID CONSTRAINT fk_expenses_sub_category REFERENCES expense_sub_categories(id) ON DELETE SET NULL,
@@ -38,10 +38,10 @@ CREATE TABLE expenses (
 	amount NUMERIC(12, 2) NOT NULL CHECK (amount > 0),
 	description TEXT,
 	created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-	updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+	updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
-CREATE TABLE incomes (
+CREATE TABLE IF NOT EXISTS incomes (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER NOT NULL CONSTRAINT fk_incomes_user REFERENCES users(id) ON DELETE CASCADE,
 	sub_category_id UUID CONSTRAINT fk_incomes_sub_category REFERENCES income_sub_categories(id) ON DELETE SET NULL,
@@ -49,5 +49,5 @@ CREATE TABLE incomes (
 	amount NUMERIC(12, 2) NOT NULL CHECK (amount > 0),
 	description TEXT,
 	created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-	updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+	updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
