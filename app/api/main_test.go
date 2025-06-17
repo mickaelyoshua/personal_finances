5b1a2cae5662ddb1761d968da2c9319590f88dd3
Mickael Yoshua <mickaelyoshua@gmail.com>
2025-06-17 12:37:51 +0000

    updating code for mock test

--- app/api/main_test.go
+++ app/api/main_test.go
@@ -4,11 +4,11 @@ import (
 	"os"
 	"testing"
 
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func TestMain(m *testing.M) {
-	//gin.SetMode(gin.TestMode)
+	gin.SetMode(gin.TestMode)
 	code := m.Run()
 	// Run after all tests are done, after the m.Run() call to ensure the test user is deleted after tests
 	os.Exit(code)
--- app/api/user_test.go
+++ app/api/user_test.go
@@ -1,15 +1,14 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
-	"errors"
-	"io"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/mickaelyoshua/personal_finances/db/mock"
 	"github.com/mickaelyoshua/personal_finances/db/sqlc"
 	"github.com/mickaelyoshua/personal_finances/util"
@@ -18,27 +17,29 @@ import (
 )
 
 func TestRegister(t *testing.T) {
-	args := sqlc.CreateUserParams{
-		Name:         util.RandomName(),
-		Email:        util.RandomEmail(),
-		PasswordHash: util.RandomPassword(),
-	}
-	user := sqlc.User{
-		Name:         args.Name,
-		Email:        args.Email,
-		PasswordHash: args.PasswordHash,
-	}
+	user := randomUser(t)
 
-	testCases := []struct{
-		Name string
-		Email string
+	testCases := []struct {
+		Name       string
+		Email      string
+		body       gin.H
 		BuildStubs func(agent *mock.MockAgent)
 		CheckResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
 	}{
 		{
-			Name: "OK",
-			Email: args.Email,
+			Name:  "OK",
+			Email: user.Email,
+			body: gin.H{
+				"name":     user.Name,
+				"email":    user.Email,
+				"password": user.PasswordHash,
+			},
 			BuildStubs: func(agent *mock.MockAgent) {
+				args := sqlc.CreateUserParams{
+					Name:         util.RandomName(),
+					Email:        util.RandomEmail(),
+					PasswordHash: util.RandomPassword(),
+				}
 				agent.EXPECT().
 					CreateUser(gomock.Any(), gomock.Eq(args)).
 					Times(1).
@@ -46,17 +47,16 @@ func TestRegister(t *testing.T) {
 			},
 			CheckResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusSeeOther, recorder.Code)
-				requireBodyMatchUser(t, recorder.Body, user)
 			},
 		},
 		{
-			Name: "InternalServerError",
-			Email: args.Email,
+			Name:  "InternalServerError",
+			Email: user.Email,
 			BuildStubs: func(agent *mock.MockAgent) {
 				agent.EXPECT().
-					CreateUser(gomock.Any(), gomock.Eq(args)).
+					CreateUser(gomock.Any(), gomock.Any()).
 					Times(1).
-					Return(user, gomock.AssignableToTypeOf(errors.New("")))
+					Return(sqlc.User{}, pgx.ErrNoRows)
 			},
 			CheckResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusInternalServerError, recorder.Code)
@@ -73,12 +73,9 @@ func TestRegister(t *testing.T) {
 			agent := mock.NewMockAgent(ctrl)
 			tc.BuildStubs(agent)
 
-			urlValues := url.Values{
-				"name":     {user.Name},
-				"email":    {user.Email},
-				"password": {user.PasswordHash},
-			}
-			request, err := http.NewRequest(http.MethodPost, "auth/register", userToReader(urlValues))
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+			request, err := http.NewRequest(http.MethodPost, "/auth/register", bytes.NewReader(data))
 			require.NoError(t, err)
 
 			recorder := httptest.NewRecorder()
@@ -89,29 +86,85 @@ func TestRegister(t *testing.T) {
 	}
 }
 
-func requireBodyMatchUser(t *testing.T, body io.Reader, user sqlc.User) {
-	gotUser, err := readerToUser(body)
+func TestLogin(t *testing.T) {
+	args := sqlc.CreateUserParams{
+		Name:         util.RandomName(),
+		Email:        util.RandomEmail(),
+		PasswordHash: util.RandomPassword(),
+	}
+	user := sqlc.User{
+		Name:         args.Name,
+		Email:        args.Email,
+		PasswordHash: args.PasswordHash,
+	}
+
+	testCases := []struct {
+		Name string
+		body gin.H
+		BuildStubs func(agent *mock.MockAgent)
+		CheckResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			Name: "OK",
+			body: gin.H{
+				"email":    user.Email,
+				"password": user.PasswordHash,
+			},
+			BuildStubs: func(agent *mock.MockAgent) {
+				agent.EXPECT().
+					GetUserByEmail(gomock.Any(), gomock.Eq(user.Email)).
+					Times(1).
+					Return(user, nil)
+			},
+			CheckResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusSeeOther, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			agent := mock.NewMockAgent(ctrl)
+			tc.BuildStubs(agent)
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+			request, err := http.NewRequest(http.MethodPost, "/auth/login", bytes.NewReader(data))
+			require.NoError(t, err)
+
+			recorder := httptest.NewRecorder()
+			server := NewServer(agent)
+			server.router.ServeHTTP(recorder, request)
+			tc.CheckResponse(t, recorder)
+		})
+	}
+}
+
+func randomUser(t *testing.T) sqlc.User {
+	password := util.RandomPassword()
+	hashedPassword, err := util.HashPassword(password)
 	require.NoError(t, err)
 
-	require.Equal(t, user.Name, gotUser.Name)
-	require.Equal(t, user.Email, gotUser.Email)
-	require.Equal(t, user.PasswordHash, gotUser.PasswordHash)
-}
-
-func userToReader(urlValues url.Values) io.Reader {
-	return strings.NewReader(urlValues.Encode())
-}
-func readerToUser(body io.Reader) (sqlc.User, error) {
-	data, err := io.ReadAll(body)
-	if err != nil {
-		return sqlc.User{}, err
+	return sqlc.User{
+		Name:         util.RandomName(),
+		Email:        util.RandomEmail(),
+		PasswordHash: hashedPassword,
 	}
-
-	var user sqlc.User
-	err = json.Unmarshal(data, &user)
-	if err != nil {
-		return sqlc.User{}, err
-	}
-
-	return user, nil
 }
+
+//func requireBodyMatchUser(t *testing.T, body io.Reader, user sqlc.User) {
+//	data, err := io.ReadAll(body)
+//	require.NoError(t, err)
+//
+//	var gotUser sqlc.User
+//	err = json.Unmarshal(data, &gotUser)
+//	require.NoError(t, err)
+//
+//	require.Equal(t, user.Name, gotUser.Name)
+//	require.Equal(t, user.Email, gotUser.Email)
+//	require.Equal(t, user.PasswordHash, gotUser.PasswordHash)
+//}
