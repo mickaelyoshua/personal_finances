e64f4fd8b8061a01421b21bc6a08c79794c6ca58
mickaelyoshua <mickaelyoshua@gmail.com>
2025-06-19 10:19:25 -0300

    change to a int32 id

--- app/util/random.go
+++ app/util/random.go
@@ -14,8 +14,8 @@ const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // RandomInt generates a random integer between min and max
-func RandomInt(min, max int64) int64 {
-	return min + r.Int63n(max-min+1)
+func RandomInt(min, max int32) int32 {
+	return min + r.Int31n(max-min+1)
 }
 
 // RandomString generates a random string of length n
