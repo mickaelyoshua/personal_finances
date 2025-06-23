37cb83f7de5265b0332480fa078045c6f6431fab
mickaelyoshua <mickaelyoshua@gmail.com>
2025-06-23 10:37:33 -0300

    any error with authentication redirect to login

--- app/api/middleware.go
+++ app/api/middleware.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"errors"
 	"net/http"
 	"strings"
 
@@ -19,29 +18,26 @@ func AuthMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authorizationHeader := c.GetHeader(authorizationHeaderKey)
 		if authorizationHeader == "" {
-			err := errors.New("authorization header is required")
-			c.AbortWithError(http.StatusUnauthorized, err)
+			c.Redirect(http.StatusSeeOther, "/auth/login")
 			return
 		}
 
 		fields := strings.Fields(authorizationHeader)
 		if len(fields) < 2 {
-			err := errors.New("invalid authorization header format")
-			c.AbortWithError(http.StatusUnauthorized, err)
+			c.Redirect(http.StatusSeeOther, "/auth/login")
 			return
 		}
 
 		authorizationType := strings.ToLower(fields[0])
 		if authorizationType != authorizationTypeBearer {
-			err := errors.New("unsupported authorization type")
-			c.AbortWithError(http.StatusUnauthorized, err)
+			c.Redirect(http.StatusSeeOther, "/auth/login")
 			return
 		}
 
 		accessToken := fields[1]
 		payload, err := tokenMaker.VerifyToken(accessToken)
 		if err != nil {
-			c.AbortWithError(http.StatusUnauthorized, err)
+			c.Redirect(http.StatusSeeOther, "/auth/login")
 			return
 		}
 
